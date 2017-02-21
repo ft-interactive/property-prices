1dfb0a951b6c79ff50316e4b7b6eca1ae4449eaf
Lily2point0 <lily.madar@ft.com>
2017-02-21 16:08:08 +0000

    added DOM output for most recent value at each location

--- client/components/property-calculator/property-calculator.js
+++ client/components/property-calculator/property-calculator.js
@@ -1,24 +1,55 @@
 import * as d3 from 'd3';
 
 ;(function(){
-    console.log(window.propertyData);
-    const pd = window.propertyData;
+    const pd = getRecentPropertiesByLocation(window.propertyData);
 
     var userInput = document.getElementById('propertyCalculator');
-    var svg = d3.select("#testSVG").append('svg');
+    var output = document.getElementById('output');
+    // var svg = d3.select("#testSVG").append('svg');
 
     userInput.addEventListener('submit', (e) => {
       e.preventDefault();
+      output.innerHTML = '';
       var amount = document.getElementById('amount').value;
 
       if(amount !== "") getArea(amount);
     });
 
     function getArea(value) {
-      console.log(value);
-
       for(var i in pd) {
-        console.log(Math.floor(parseInt(value)/parseInt(pd[i].value)) + 'sqmt in ' + pd[i].city);
+        var result = document.createElement("p");
+        result.textContent = value + ' ' + pd[i].currency + ' buys ' + Math.floor(parseInt(value)/parseInt(pd[i].value)) + ' sqmt in ' + pd[i].city;
+        output.appendChild(result);
       }
+    }
+
+    function getRecentPropertiesByLocation(array) {
+      var duplicateArray = sortPropertiesByLocationAndDate(array.slice());
+
+      for(var i = duplicateArray.length - 1; i > 0; --i) {
+        if(duplicateArray[i - 1].city === duplicateArray[i].city) {
+          duplicateArray.splice(i, 1);
+        }
+      }
+
+      return duplicateArray;
+    }
+
+    function sortPropertiesByLocationAndDate(array) {
+      array.sort(function(a,b){
+        if(a.city === b.city) {
+          return new Date(b.date) - new Date(a.date);
+        }
+        else {
+            var nameA=a.city.toLowerCase(), nameB=b.city.toLowerCase()
+            if (nameA < nameB) //sort string ascending
+              return -1 
+            if (nameA > nameB)
+              return 1
+            return 0;
+        }
+      });
+
+      return array;
     }
 }());

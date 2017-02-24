6454875fba1a780f93ef3bbffbd2ad3774ea5ea0
Lily2point0 <lily.madar@ft.com>
2017-02-24 11:34:17 +0000

    added autocomplete currency selection

--- client/components/property-calculator/DOMElements.js
+++ client/components/property-calculator/DOMElements.js
@@ -1,6 +1,9 @@
+import 'awesomplete';
+
 ;(function(){
-	var buttons = document.querySelectorAll('.currency-button');
+	var buttons = document.querySelectorAll('.currency-button:not(.currency-input)');
 	var currency = document.getElementById('currencyInput');
+	var completeInput = document.querySelector('input.awesomplete');
 
 	buttons.forEach(function(button){
 		button.addEventListener('click', function(e){
@@ -11,9 +14,17 @@
 		});
 	});
 
+	completeInput.addEventListener('awesomplete-selectcomplete', function(e){
+		resetCurrencySelection(null);
+		e.currentTarget.classList.add('selected');
+		currency.value= completeInput.value;
+	});
+
 	function resetCurrencySelection(target) {
 		buttons.forEach(function(button){
 			if(button !== target) button.classList.remove('selected');
 		});
+
+		completeInput.classList.remove('selected');
 	}
 }());

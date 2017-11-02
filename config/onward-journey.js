5072068d4a98780167ad9ed4f5d4919eebc07942
Martin Stabe <martinstabe@gmail.com>
2017-11-02 13:49:44 +0000

    Update copy and OJ config

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,5 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 2, list: 'thing/MjNiMDgwZDktMTQwYy00NmY4LTk1MWMtZDM4NWMxNTkwNTQ5-VG9waWNz' },
+    { rows: 2, list: 'thing/4f1c6ab6-f45d-30b0-9e04-47219c03da5e' },
   ],
 });

f52c9b8638e773996eca9335e3b1715dd4f03256
Callum Locke <callum.locke@gmail.com>
2017-11-03 15:10:15 +0000

    Update copy

--- config/article.js
+++ config/article.js
@@ -11,7 +11,7 @@ export default () => ({ // eslint-disable-line
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'Prime property price calculator',
+  headline: 'Global prime property calculator',
 
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'How much prime residential property could you buy in the world’s priciest cities?',
